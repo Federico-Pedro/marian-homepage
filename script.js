592cc81fea41d736c49243daf28841c18dc25c84
Federico-Pedro <federicopedroroveda@gmail.com>
2024-12-12 19:34:21 -0300

    Creates carousel in works section

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentIndex = 0;
+
+document.querySelector('.prev-button').addEventListener('click', () => {
+    navigate(-1);
+});
+
+document.querySelector('.next-button').addEventListener('click', () => {
+    navigate(1);
+});
+
+function navigate(direction) {
+    const items = document.querySelectorAll('.gallery-item');
+    const totalImages = items.length;
+    
+    items[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + direction + totalImages) % totalImages;
+    items[currentIndex].classList.add('active');
+}
+
+// Inicializar la galería mostrando la primera imagen
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.gallery-item');
+    if (items.length > 0) {
+        items[currentIndex].classList.add('active');
+    }
+});
+
