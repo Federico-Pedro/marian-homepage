d58f8e0b97ea1a3cb40a7ed126441a3acd087dfa
Federico-Pedro <federicopedroroveda@gmail.com>
2024-12-13 18:39:05 -0300

    makes menu fully functional

--- script2.js
+++ script2.js
@@ -1,7 +1,27 @@
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('.nav-bar');
+const x = document.querySelector('.close');
 
 menu.addEventListener('click', () =>{
     nav.classList.toggle('show');
+	x.classList.toggle('show');
 });
 
+
+x.onclick = function() {
+  
+    nav.classList.remove('show');
+	x.classList.remove('show');
+};
+
+
+document.addEventListener("keydown", function() {
+
+  if (event.key === "Escape") {
+
+    nav.classList.remove('show');
+	x.classList.remove('show');
+
+  }
+
+});
