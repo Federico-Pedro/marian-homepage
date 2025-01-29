0f944ba562f62a4542fa8d05fb379c2617dd6c67
Federico-Pedro <federicopedroroveda@gmail.com>
2025-01-29 20:55:25 -0300

    Organizes web site structure and folders

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.menu');
 const nav = document.querySelector('.nav-bar');
 const x = document.querySelector('.close');
 
+
 menu.addEventListener('click', () =>{
     nav.classList.toggle('show');
 	x.classList.toggle('show');
@@ -20,7 +21,7 @@ document.addEventListener("keydown", function() {
   if (event.key === "Escape") {
 
     nav.classList.remove('show');
-	x.classList.remove('show');
+	  x.classList.remove('show');
 
   }
 
